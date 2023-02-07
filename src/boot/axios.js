8af0c281430bf32440a4a7b6f4754d172561e2b4
Ethan0Stevens <ethan.stevens@divtec.ch>
2023-02-07 15:50:39 +0100

    Creation de l'accesseur a l'api avec axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,24 @@
 import { boot } from 'quasar/wrappers'
+import axios from 'axios'
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
-  // something to do
+const api = axios.create({
+  baseURL: 'http://tempapi.divtec.me/api',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 })
+
+export default boot(({ app }) => {
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+
+  app.config.globalProperties.$api = api
+  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
+  //       so you can easily perform requests against your app's API
+})
+
+export { api }
