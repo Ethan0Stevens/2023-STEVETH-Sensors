7493288d7a370502f4e44b1af275d72ad7270b33
Ethan0Stevens <ethan.stevens@divtec.ch>
2023-02-07 15:27:31 +0100

    Design du formulaire de connexion

--- src/pages/ConnexionPage.vue
+++ src/pages/ConnexionPage.vue
@@ -3,32 +3,33 @@
     <q-img :fit="'fill'" height="100vh" src="https://getwallpapers.com/wallpaper/full/8/7/3/1089718-one-piece-crew-wallpaper-1920x1080-for-full-hd.jpg">
       <div class="absolute-full flex flex-center">
         <q-card class="my-card">
-          <q-card-section>
-            <q-form
-              @submit="onSubmit"
-              @reset="onReset"
-              class="q-gutter-md" >
-              <q-input
-                filled
-                label="Adresse e-mail *"
-                lazy-rules
-                :rules="[ val => val && val.length > 0 || 'Champ obligatoire']" />
+          <div class="q-pa-xl">
+            <q-card-section>
+              <div class="text-bold text-black text-h3" align="center">Connexion</div>
+              <q-separator class="bg-primary"/>
+            </q-card-section>
+            <q-card-section>
+              <q-form
+                @submit="onSubmit"
+                @reset="onReset" >
+                <q-input
+                  filled
+                  type="email"
+                  label="Adresse e-mail *"
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Champ obligatoire']" />
 
-              <q-input
-                filled
-                label="Mot de passe *"
-                lazy-rules
-                :rules="[
-                  val => val !== null && val !== '' || 'Please type your age',
-                  val => val > 0 && val < 100 || 'Please type a real age'
-                ]" />
+                <q-input
+                  filled
+                  type="password"
+                  label="Mot de passe *" />
 
-              <div>
-                <q-btn label="Submit" type="submit" color="primary"/>
-                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-              </div>
-            </q-form>
-          </q-card-section>
+                <div class="q-pt-lg" align="center" >
+                  <q-btn size="lg" icon="arrow_forward" type="submit" color="primary" round />
+                </div>
+              </q-form>
+            </q-card-section>
+          </div>
         </q-card>
       </div>
     </q-img>
