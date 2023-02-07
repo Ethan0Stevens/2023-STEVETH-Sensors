5e0c7b750806f902578a496b4fb0a5bb708c8a1b
Ethan0Stevens <ethan.stevens@divtec.ch>
2023-02-07 14:58:15 +0100

    Ajout de la page de connexion

--- src/pages/ConnexionPage.vue
+++ src/pages/ConnexionPage.vue
@@ -1,6 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Connexion</h1>
+  <q-page class="fullscreen" >
+    <q-img :fit="'fill'" height="100vh" src="https://getwallpapers.com/wallpaper/full/8/7/3/1089718-one-piece-crew-wallpaper-1920x1080-for-full-hd.jpg">
+      <div class="absolute-full flex flex-center">
+        <q-card class="my-card">
+          <q-card-section>
+            <q-form
+              @submit="onSubmit"
+              @reset="onReset"
+              class="q-gutter-md" >
+              <q-input
+                filled
+                label="Adresse e-mail *"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Champ obligatoire']" />
+
+              <q-input
+                filled
+                label="Mot de passe *"
+                lazy-rules
+                :rules="[
+                  val => val !== null && val !== '' || 'Please type your age',
+                  val => val > 0 && val < 100 || 'Please type a real age'
+                ]" />
+
+              <div>
+                <q-btn label="Submit" type="submit" color="primary"/>
+                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-img>
   </q-page>
 </template>
 
