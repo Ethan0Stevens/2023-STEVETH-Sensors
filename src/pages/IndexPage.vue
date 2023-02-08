fac4efba4e40305c66fca8395c452be9bcf510f1
Ethan0Stevens <ethan.stevens@divtec.ch>
2023-02-08 17:18:06 +0100

    Ajout d'une image sur la page d'accueil

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <q-img class="fixed-bottom" height="100vh" src="https://imgs.search.brave.com/Per6nrxDjMRlOEWGFqQ2SNMGXwGtXquuuv35yvG8_3E/rs:fit:1200:1080:1/g:ce/aHR0cDovL3d3dy53/YWxscGFwZXJtYWlk/ZW4uY29tL2ltYWdl/LzIwMTcvMTEvMjUv/b25lLXBpZWNlLXRo/b3VzYW5kLXN1bm55/LXNoaXAtb2NlYW4t/Y2xvdWRzLWFydHdv/cmstYW5pbWUtMTg2/OTUuanBn">
+      <div class="absolute-full flex flex-center">
+      </div>
+    </q-img>
   </q-page>
 </template>
 
