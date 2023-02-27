6500508871cd5d6e226d4c9e509560b55d025894
Ethan0Stevens <ethan.stevens@divtec.ch>
2023-02-27 09:32:50 +0100

    Ajout d'une video de fond sur la page d'accueil

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,8 @@
 <template>
   <q-page>
-    <q-img class="fixed-bottom" height="100vh" src="https://imgs.search.brave.com/Per6nrxDjMRlOEWGFqQ2SNMGXwGtXquuuv35yvG8_3E/rs:fit:1200:1080:1/g:ce/aHR0cDovL3d3dy53/YWxscGFwZXJtYWlk/ZW4uY29tL2ltYWdl/LzIwMTcvMTEvMjUv/b25lLXBpZWNlLXRo/b3VzYW5kLXN1bm55/LXNoaXAtb2NlYW4t/Y2xvdWRzLWFydHdv/cmstYW5pbWUtMTg2/OTUuanBn">
-      <div class="absolute-full flex flex-center">
-      </div>
-    </q-img>
+    <video class="fixed-center" src="../assets/one-piece.mp4" autoplay muted style="width: 100vw;">
+
+    </video>
   </q-page>
 </template>
 
